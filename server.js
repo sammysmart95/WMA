f972fafee25fcddf697df44a6c805c5bd7ca65e2
?ranky <sammysmart95@gmail.com>
2019-11-07 20:23:21 +0100

    added dotenv file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+
 app.use(express.static('src'))
 
-app.listen(process.env.port || 8080, ()=> 
-console.log("All is fine")
+app.listen(process.env.PORT || 8080, ()=> 
+console.log(`App is running in port ${process.env.PORT}`)
 )
